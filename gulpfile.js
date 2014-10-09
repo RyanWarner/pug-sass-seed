4eb4a68e2ce6cb2b3c5dbb2578b0293025954bb2
Ryan Warner <warner.ryan@gmail.com>
2014-10-09 11:41:36 -0700

    cleaning up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ var scsslint 	= require( 'gulp-scss-lint' );
 var csscomb 	= require( 'gulp-csscomb' );
 var eslint      = require( 'gulp-eslint' );
 
+
+
 // File locations.
 
 var OUTPUT_DIR 	= 'build/';
@@ -40,20 +42,30 @@ gulp.task( 'watch', function(  )
 
 gulp.task( 'clean', function(  )
 {
-	return gulp.src( OUTPUT_DIR,
+	return
+	{
+		gulp.src( OUTPUT_DIR,
 		{
 			read: false
 		} )
 		.pipe( clean(  ) );
+	};
 } );
+
+
+
+// Jade
 
 gulp.task( 'jade', function(  )
 {
-	return gulp.src( JADE_FILES )
+	return
+	{
+		gulp.src( JADE_FILES )
 		.pipe( jade(  ) )
 		.on( 'error', handleError )
 		.pipe( gulp.dest( OUTPUT_DIR ) )
 		.pipe( connect.reload(  ) );
+	};
 } );
 
 
@@ -62,27 +74,35 @@ gulp.task( 'jade', function(  )
 
 gulp.task( 'csscomb', function (  )
 {
-    return gulp.src( SASS_FILES )
+    return
+    {	gulp.src( SASS_FILES )
         .pipe( csscomb(  ) )
         .pipe( gulp.dest( 'sass' ) );
+    };
 } );
 
 gulp.task( 'scss-lint', [ 'csscomb' ], function(  )
 {
-	return gulp.src( SASS_FILES )
+	return
+	{
+		gulp.src( SASS_FILES )
     	.pipe( scsslint( { 'config': 'scss-linting-config.yml' } ) )
     	.on( 'error', handleError );
+    };
 } );
 
 gulp.task( 'sass', [ 'scss-lint' ], function(  )
 {
-	return gulp.src( SASS_FILES )
+	return
+	{
+		gulp.src( SASS_FILES )
 		.pipe( sass(  ) )
 		.on( 'error', handleError )
 		.pipe( prefix( "last 2 versions", { cascade: true } ) )
 		.on( 'error', handleError )
 		.pipe( gulp.dest( OUTPUT_DIR + 'css' ) )
 		.pipe( connect.reload(  ) );
+	};
 } );
 
 
@@ -91,31 +111,50 @@ gulp.task( 'sass', [ 'scss-lint' ], function(  )
 
 gulp.task( 'eslint', function(  )
 {
-    return gulp.src( SCRIPT_FILES )
+    return
+    {
+    	gulp.src( SCRIPT_FILES )
         .pipe( eslint(  ) )
         .pipe( eslint.format(  ) );
+    };
 } );
 
 gulp.task( 'js', [ 'eslint' ], function(  )
 {
-	return gulp.src( SCRIPT_FILES )
+	return
+	{
+		gulp.src( SCRIPT_FILES )
 		.pipe( gulp.dest( OUTPUT_DIR + 'scripts' ) )
 		.pipe( connect.reload(  ) );
+	};
 } );
+
+
+
+// Assets
 
 gulp.task( 'images', function(  )
 {
-	return gulp.src( IMAGE_FILES )
+	return
+	{
+		gulp.src( IMAGE_FILES )
 		.pipe( gulp.dest( OUTPUT_DIR + '/images/' ) )
 		.pipe( connect.reload(  ) );
+	};
 } );
 
 gulp.task( 'favicon', function(  )
 {
-	return gulp.src( FAVICON )
+	return
+	{
+		gulp.src( FAVICON )
 		.pipe( gulp.dest( OUTPUT_DIR ) )
 		.pipe( connect.reload(  ) );
+	};
 } );
+
+
+
 
 gulp.task( 'connect', function(  )
 {
@@ -134,9 +173,16 @@ gulp.task( 'open', function(  )
 		app: "google chrome"
 		//app: "safari"
 	};
-	return gulp.src( OUTPUT_DIR + "index.html" )
+
+	return
+	{
+		gulp.src( OUTPUT_DIR + "index.html" )
 		.pipe( open( "http://localhost:8080", options ) );
+	};
 } );
+
+
+
 
 gulp.task( 'default', function(  )
 {
